baf80bad2243765dba2b7bc29d6362b10e3d6227
Cadet <cadet@cadet-vm.ninja>
2019-04-09 11:34:09 -0400

    Got models working

---
+++ mysite/polls/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+class academic_dept(models.Model):
+    dept_code = models.CharField(max_length=10,unique=True)
+    dept_name = models.CharField(max_length=100)
+
+class academic_class(models.Model):
+    class_code = models.CharField(max_length=5, unique=True)
+    dept_code = models.ForeignKey(
+        'academic_dept',
+        on_delete=models.CASCADE,
+    )
+    class_name = models.CharField(max_length=30)
+
+class file(models.Model):
+    file_alias = models.CharField(max_length=30, unique=True)
+    class_code = models.ForeignKey(
+        'academic_class',
+        on_delete=models.CASCADE,
+    )
+    document = models.FileField(upload_to='documents/')
+    rating = models.IntegerField(null=True)
+    downloads = models.IntegerField(default=0)
+    flags = models.IntegerField(default=0)
+    upload_date = models.DateTimeField(auto_now=True)
+
+    
+
+
